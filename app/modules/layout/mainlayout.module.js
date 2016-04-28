897535a441ae705aaaeae6cd6e585f08080874c4
kotesh79 <vrkeats@yahoo.com>
2016-04-28 23:32:45 +0530

    Added files via upload

--- app/modules/layout/mainlayout.module.js
+++ app/modules/layout/mainlayout.module.js
@@ -1,1 +1,35 @@
-
+/**
+ * @ngdoc object
+ * @description
+ *
+ */
+(function() {
+    'use strict';
+    angular.module('ktoolsApp.mainlayout', [])
+        .config(['$stateProvider', function($stateProvider) {
+            $stateProvider
+                .state('index', {
+                    abstract: false,
+                    views: {
+                        'header@': {
+                            controller: 'MainHeaderCtrl',
+                            templateUrl: 'modules/navigation/header.fixedtop.html'
+                        },
+                        'left-nav@': {
+                            controller: 'MainMenuCtrl',
+                            templateUrl: 'modules/navigation/mainmenu.html'
+                        },
+                        'footer@': {
+                            controller: 'MainFooterCtrl',
+                            templateUrl: 'modules/navigation/footer.template.html'
+                        }
+                    },
+                    data: {
+                        pageMainTitle: null,
+                        pageSubTitle: null,
+                        displayName: 'Index',
+                        bodyClassName: ''
+                    }
+                });
+        }]);
+})();
