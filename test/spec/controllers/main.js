42fa17de6f5383f89f7cfb6e9931fc8328a28050
kotesh79 <vrkeats@yahoo.com>
2016-04-28 22:46:48 +0530

    Added files via upload

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,17 +1,17 @@
 'use strict';
 
-describe('Controller: AboutCtrl', function () {
+describe('Controller: MainCtrl', function () {
 
   // load the controller's module
   beforeEach(module('simple7WsApp'));
 
-  var AboutCtrl,
+  var MainCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    AboutCtrl = $controller('AboutCtrl', {
+    MainCtrl = $controller('MainCtrl', {
       $scope: scope
     });
   }));
