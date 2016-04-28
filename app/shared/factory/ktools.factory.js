cde72233cdbc78509aceaf905ee7cab219938045
kotesh79 <vrkeats@yahoo.com>
2016-04-28 23:13:26 +0530

    Added files via upload

--- app/shared/factory/ktools.factory.js
+++ app/shared/factory/ktools.factory.js
@@ -1,1 +1,20 @@
+angular.module('ktoolsApp')
+  .factory('alert', function($uibModal) {
 
+    function show(action, event) {
+      return $uibModal.open({
+        templateUrl: 'shared/templates/modal.alert.template.html',
+        controller: function() {
+          var vm = this;
+          vm.action = action;
+          vm.event = event;
+        },
+        controllerAs: 'vm'
+      });
+    }
+
+    return {
+      show: show
+    };
+
+  });
